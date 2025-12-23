7a1f425d19e8ae036bd43c1fd072264b5f463e52
Ved1234566 <vedangsharma39@gmail.com>
2025-12-23 12:27:54 +0530

    Update ML datasets and reorganize files
    Added new churn modelling and insurance CSV datasets, removed outdated datasets and scripts, and reorganized ML scripts and test files for improved project structure.

--- ML/ML/3.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv("amazon.csv")
-print(df)
-
-# 2. Check if there are missing values are or not ? If there are then handle missing values . 
-df.isnull().sum()
-df
